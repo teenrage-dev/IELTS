8818ea1ade8dbac8aa04f45a9c4cb10c80291d6e
teenrage-dev <72465313+teenrage-dev@users.noreply.github.com>
2022-10-05 16:46:19 +0000

    Deploying to gh-pages from @ teenrage-dev/IELTS@0fec8ddc294e9f9734bf6f684446735ab3ba7457 🚀

---
+++ index.07577bc4.js
@@ -0,0 +1,2 @@
+document.querySelector(".services-item-form").addEventListener("change",(function({target:e}){document.querySelectorAll(".services-radio-wrapper").forEach((e=>{e.classList.remove("active")})),e.closest(".services-radio-wrapper").classList.add("active")}));
+//# sourceMappingURL=index.07577bc4.js.map
---
+++ index.f1980ff7.js
@@ -0,0 +1,2 @@
+document.querySelector(".services-item-form").addEventListener("change",(function(e){var r=e.target;document.querySelectorAll(".services-radio-wrapper").forEach((function(e){e.classList.remove("active")})),r.closest(".services-radio-wrapper").classList.add("active")}));
+//# sourceMappingURL=index.f1980ff7.js.map
